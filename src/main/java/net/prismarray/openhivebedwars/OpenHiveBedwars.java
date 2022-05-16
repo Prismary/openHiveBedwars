ff341c37bd3a59b69044d82b88f042e599d5e28d
Clemens Nowitzki <clemens@nowitzki.org>
2022-05-16 19:46:57 +0200

    Initial commit

---
+++ src/main/java/net/prismarray/openhivebedwars/OpenHiveBedwars.java
@@ -0,0 +1,17 @@
+package net.prismarray.openhivebedwars;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class OpenHiveBedwars extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
