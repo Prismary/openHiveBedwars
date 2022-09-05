e14c514664f6ae7bc1663a637a2a7ab48deeed4a
Paul Nowitzki <paul@nowitzki.org>
2022-09-06 00:54:03 +0200

    implemented MapManager

--- src/main/java/net/prismarray/openhivebedwars/config/MapManager.java
+++ src/main/java/net/prismarray/openhivebedwars/config/MapManager.java
@@ -1,21 +1,21 @@
 package net.prismarray.openhivebedwars.config;
 
 import dev.dejvokep.boostedyaml.YamlDocument;
-import org.bukkit.World;
+import dev.dejvokep.boostedyaml.settings.dumper.DumperSettings;
+import dev.dejvokep.boostedyaml.settings.general.GeneralSettings;
+import org.apache.commons.io.FilenameUtils;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.File;
-import java.io.FileFilter;
-import java.io.FilenameFilter;
-import java.util.Arrays;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Scanner;
 
 public class MapManager {
 
     private final JavaPlugin plugin;
-    private Map<String, YamlDocument> mapConfigs;
+    private final Map<String, MapConfig> mapConfigs;
 
     public MapManager(JavaPlugin plugin) {
 
@@ -27,16 +27,78 @@ public class MapManager {
 
         this.mapConfigs.clear();
 
-        File[] mapConfigFiles = getYMLFilesInDirectory(this.plugin.getDataFolder());
+        File mapsDirectory = new File(this.plugin.getDataFolder(), "maps");
+
+        if (!mapsDirectory.exists()) {
+
+            this.plugin.getLogger().info(
+                    "Maps directory '" + mapsDirectory.getPath() + "' does not exist. " +
+                            "Creating a new one with an example config..."
+            );
+            mapsDirectory.mkdirs();
+
+            try {
+                File sampleConfigFile = new File(mapsDirectory, "sample_map.yml");
+                sampleConfigFile.createNewFile();
+
+                Scanner scanner = new Scanner(this.plugin.getResource("sample_map.yml"));
+                StringBuilder strb = new StringBuilder();
+
+                while (scanner.hasNext()) {
+                    strb.append(scanner.nextLine() + "\n");
+                }
+
+                FileWriter writer = new FileWriter(sampleConfigFile);
+                writer.write(strb.toString());
+                writer.close();
+
+            } catch (IOException e) {
+                this.plugin.getLogger().warning(
+                        "Could not create sample config due to IOException: " + e.getMessage()
+                );
+            }
+        }
+
+        //TODO: figure out, why this doesn't load anymore...
+
+        File[] mapConfigFiles = getYMLFilesInDirectory(mapsDirectory);
 
         if (Objects.isNull(mapConfigFiles) || mapConfigFiles.length < 1) {
+            this.plugin.getLogger().warning(
+                    "Could not find any map configuration files in '" + mapsDirectory.getPath() + "'."
+            );
             return;
         }
 
         for (File configFile : mapConfigFiles) {
-            YamlDocument.create(configFile, this.plugin.getResource("sample_map.yml"));
-            //TODO: continue here
+
+            String mapID = FilenameUtils.removeExtension(configFile.getName());
+
+            try {
+                MapConfig mapConfig = new MapConfig(this.plugin.getLogger(), configFile);
+                mapConfig.loadConfig();
+                mapConfigs.put(mapID, mapConfig);
+
+            } catch (IOException e) {
+                this.plugin.getLogger().warning(
+                        "Could not parse config file '" + configFile.getName() + "'. Skipping..."
+                );
+
+            } catch (IllegalArgumentException e) {
+                this.plugin.getLogger().warning(
+                        "Could not parse config file '" + configFile.getName() + "'. Skipping..."
+                );
+                this.plugin.getLogger().warning("Error message: " + e.getMessage());
+            }
         }
+    }
+
+    public MapConfig getMapConfig(String mapID) {
+        return mapConfigs.get(mapID);
+    }
+
+    public void clear() {
+        mapConfigs.clear();
     }
 
     private static File[] getYMLFilesInDirectory(File directory) {
