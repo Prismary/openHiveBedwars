6087d9979e602fe95ea0355251aa4651af338077
BuildTools <unconfigured@null.spigotmc.org>
2022-09-09 19:01:57 +0200

    added config attributes and implemented parser and validation

--- src/main/java/net/prismarray/openhivebedwars/config/LobbyConfig.java
+++ src/main/java/net/prismarray/openhivebedwars/config/LobbyConfig.java
@@ -1,24 +1,21 @@
 package net.prismarray.openhivebedwars.config;
 
 import dev.dejvokep.boostedyaml.YamlDocument;
+import org.bukkit.Location;
 
 import java.io.File;
 import java.util.logging.Logger;
 
 public class LobbyConfig extends ConfigFile {
 
-    /*
-    TODO: implement the following config options:
+    private Location lobbyPlayerSpawnLocation;
+    private Location resultsPlayerSpawnLocation;
+    private Location scoreboardLocation;
 
-        - lobby player spawn
-        - scoreboard location
-        - results player spawn
-        - podium gold location
-        - podium silver location
-        - podium bronze location
-     */
+    private Location podiumGoldLocation;
+    private Location podiumSilverLocation;
+    private Location podiumBronzeLocation;
 
-    //TODO: instantiate this config in OpenHiveBedwars (on enable)
 
     public LobbyConfig(Logger logger, File configFile) {
         super(logger, configFile);
@@ -27,5 +24,36 @@ public class LobbyConfig extends ConfigFile {
     @Override
     protected void parseAndValidateConfig(YamlDocument yamlContent) throws ConfigValidationException {
 
+        this.lobbyPlayerSpawnLocation = parseLocation(yamlContent.getString("locations.lobby_player_spawn"));
+        this.resultsPlayerSpawnLocation = parseLocation(yamlContent.getString("locations.results_player_spawn"));
+        this.scoreboardLocation = parseLocation(yamlContent.getString("locations.scoreboard"));
+
+        this.podiumGoldLocation = parseLocation(yamlContent.getString("locations.podium.gold"));
+        this.podiumSilverLocation = parseLocation(yamlContent.getString("locations.podium.silver"));
+        this.podiumBronzeLocation = parseLocation(yamlContent.getString("locations.podium.bronze"));
+    }
+
+    public Location getLobbyPlayerSpawnLocation() {
+        return lobbyPlayerSpawnLocation;
+    }
+
+    public Location getResultsPlayerSpawnLocation() {
+        return resultsPlayerSpawnLocation;
+    }
+
+    public Location getScoreboardLocation() {
+        return scoreboardLocation;
+    }
+
+    public Location getPodiumGoldLocation() {
+        return podiumGoldLocation;
+    }
+
+    public Location getPodiumSilverLocation() {
+        return podiumSilverLocation;
+    }
+
+    public Location getPodiumBronzeLocation() {
+        return podiumBronzeLocation;
     }
 }
