587dcaad2fc3d56f0e21626b8361c6de79b95838
Paul Nowitzki <p.nowitzki@tu-bs.de>
2023-01-02 01:36:40 +0100

    fixed event handler for inventory GUI actions

--- src/main/java/net/prismarray/openhivebedwars/events/EvtInventoryGUI.java
+++ src/main/java/net/prismarray/openhivebedwars/events/EvtInventoryGUI.java
@@ -3,21 +3,14 @@ package net.prismarray.openhivebedwars.events;
 import net.prismarray.openhivebedwars.OpenHiveBedwars;
 import net.prismarray.openhivebedwars.gui.InventoryGUIActionManager;
 import net.prismarray.openhivebedwars.gui.InventoryGUIBase;
-import net.prismarray.openhivebedwars.gui.InventoryGUIItem;
 import net.prismarray.openhivebedwars.gui.actions.*;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
-import org.bukkit.event.inventory.ClickType;
-import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryCloseEvent;
-import org.bukkit.event.inventory.InventoryInteractEvent;
-import org.bukkit.inventory.ItemStack;
+import org.bukkit.event.inventory.*;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 public class EvtInventoryGUI extends EventBase {
 
@@ -28,21 +21,21 @@ public class EvtInventoryGUI extends EventBase {
     @EventHandler(priority = EventPriority.LOWEST)
     public void onInventoryClose(InventoryCloseEvent e) {
 
-        if (!(e.getInventory() instanceof InventoryGUIBase)) {
+        if (!InventoryGUIActionManager.isRegistered(e.getInventory())) {
             return;
         }
 
-        InventoryGUIBase invGUI = (InventoryGUIBase) e.getInventory();
+        InventoryGUIBase invGUI = InventoryGUIActionManager.getCorrespondingInventoryGUIBase(e.getInventory());
         Player player = (Player) e.getPlayer();
 
         InventoryGUIActionManager.handleInventoryGUIAction(new InventoryGUICloseAction(invGUI, player));
         InventoryGUIActionManager.unregisterInventoryGUI(invGUI);
     }
 
-    @EventHandler(priority = EventPriority.LOWEST)
-    public void onInventoryInteraction(InventoryInteractEvent e) {
+    @EventHandler
+    public void onInventoryDrag(InventoryDragEvent e) {
 
-        if (!(e.getInventory() instanceof InventoryGUIBase)) {
+        if (!InventoryGUIActionManager.isRegistered(e.getInventory())) {
             return;
         }
 
@@ -50,53 +43,74 @@ public class EvtInventoryGUI extends EventBase {
             return;
         }
 
-        InventoryGUIBase invGUI = (InventoryGUIBase) e.getInventory();
-        Player player = (Player) e.getWhoClicked();
+        InventoryGUIBase invGUI = InventoryGUIActionManager.getCorrespondingInventoryGUIBase(e.getInventory());
 
-        if (invGUI.isLocked()) {
+        if (invGUI.isLocked() || e.getInventorySlots().stream().anyMatch(invGUI::isSlotLocked)) {
             e.setCancelled(true);
             e.setResult(Event.Result.DENY);
         }
+    }
 
-        if (!(e instanceof InventoryClickEvent)) {
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onInventoryInteraction(InventoryClickEvent e) {
+
+        if (!InventoryGUIActionManager.isRegistered(e.getClickedInventory())) {
             return;
         }
 
-        InventoryClickEvent eClick = (InventoryClickEvent) e;
-        int invSlot = eClick.getSlot();
-        ItemStack clickedItem = invGUI.getItem(invSlot);
+        if (e.isCancelled() || Objects.equals(e.getResult(), Event.Result.DENY)) {
+            return;
+        }
 
-        if (Objects.nonNull(clickedItem) && clickedItem instanceof InventoryGUIItem && ((InventoryGUIItem) clickedItem).isLocked()) {
+        InventoryGUIBase invGUI = InventoryGUIActionManager.getCorrespondingInventoryGUIBase(e.getClickedInventory());
+        Player player = (Player) e.getWhoClicked();
+        int invSlot = e.getSlot();
+
+        if (invGUI.isLocked() || invGUI.isSlotLocked(invSlot)) {
             e.setCancelled(true);
             e.setResult(Event.Result.DENY);
         }
 
-        List<InventoryGUIAction> actions = new ArrayList<>();
+        List<InventoryGUIAction> actions;
 
-        if (eClick.isLeftClick()) {
+        switch (e.getClick()) {
+            case LEFT:
+                actions = Arrays.asList(
+                        new InventoryGUILeftOrShiftLeftClickAction(invGUI, player, invSlot),
+                        new InventoryGUILeftClickAction(invGUI, player, invSlot)
+                );
+                break;
 
-            actions.add(new InventoryGUILeftOrShiftLeftClickAction(invGUI, player, invSlot));
+            case SHIFT_LEFT:
+                actions = Arrays.asList(
+                        new InventoryGUILeftOrShiftLeftClickAction(invGUI, player, invSlot),
+                        new InventoryGUIShiftLeftClickAction(invGUI, player, invSlot)
+                );
+                break;
 
-            if (eClick.isShiftClick()) {
-                actions.add(new InventoryGUIShiftLeftClickAction(invGUI, player, invSlot));
-            } else {
-                actions.add(new InventoryGUILeftClickAction(invGUI, player, invSlot));
-            }
-        }
+            case RIGHT:
+                actions = Arrays.asList(
+                        new InventoryGUIRightOrShiftRightClickAction(invGUI, player, invSlot),
+                        new InventoryGUIRightClickAction(invGUI, player, invSlot)
+                );
+                break;
 
-        if (eClick.isRightClick()) {
+            case SHIFT_RIGHT:
+                actions = Arrays.asList(
+                        new InventoryGUIRightOrShiftRightClickAction(invGUI, player, invSlot),
+                        new InventoryGUIShiftRightClickAction(invGUI, player, invSlot)
+                );
+                break;
 
-            actions.add(new InventoryGUIRightOrShiftRightClickAction(invGUI, player, invSlot));
+            case MIDDLE:
+                actions = Collections.singletonList(
+                        new InventoryGUIMiddleClickAction(invGUI, player, invSlot)
+                );
+                break;
 
-            if (eClick.isShiftClick()) {
-                actions.add(new InventoryGUIShiftRightClickAction(invGUI, player, invSlot));
-            } else {
-                actions.add(new InventoryGUIRightClickAction(invGUI, player, invSlot));
-            }
-        }
-
-        if (Objects.equals(eClick.getClick(), ClickType.MIDDLE)) {
-            actions.add(new InventoryGUIMiddleClickAction(invGUI, player, invSlot));
+            default:
+                actions = new ArrayList<>();
+                break;
         }
 
         actions.forEach(InventoryGUIActionManager::handleInventoryGUIAction);
