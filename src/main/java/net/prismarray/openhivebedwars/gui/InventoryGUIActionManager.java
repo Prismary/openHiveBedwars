c73b207be4959dbed9a28ac689090e9e913b0cdf
Paul Nowitzki <p.nowitzki@tu-bs.de>
2023-01-02 01:35:53 +0100

    fixed Action handling in InventoryGUIActionManager

--- src/main/java/net/prismarray/openhivebedwars/gui/InventoryGUIActionManager.java
+++ src/main/java/net/prismarray/openhivebedwars/gui/InventoryGUIActionManager.java
@@ -1,16 +1,15 @@
 package net.prismarray.openhivebedwars.gui;
 
 import net.prismarray.openhivebedwars.gui.actions.InventoryGUIAction;
+import org.bukkit.inventory.Inventory;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 public class InventoryGUIActionManager {
 
     private static final InventoryGUIActionManager instance = new InventoryGUIActionManager();
 
-    private List<InventoryGUIBase> openInventoryGUIs = new ArrayList<>();
+    private final Map<Inventory, InventoryGUIBase> openInventoryGUIs = new HashMap<>();
 
     private InventoryGUIActionManager() {}
 
@@ -19,16 +18,26 @@ public class InventoryGUIActionManager {
     }
 
     public static void registerInventoryGUI(InventoryGUIBase invGUI) {
-        instance.openInventoryGUIs.add(invGUI);
+        instance.openInventoryGUIs.put(invGUI.getInventory(), invGUI);
+    }
+
+    public static boolean isRegistered(Inventory inventory) {
+        return instance.openInventoryGUIs.containsKey(inventory);
+    }
+
+    public static boolean isRegistered(InventoryGUIBase invGUI) {
+        return instance.openInventoryGUIs.containsKey(invGUI.getInventory());
     }
 
     public static boolean unregisterInventoryGUI(InventoryGUIBase invGUI) {
-        return instance.openInventoryGUIs.remove(invGUI);
+        return Objects.nonNull(instance.openInventoryGUIs.remove(invGUI.getInventory()));
+    }
+
+    public static InventoryGUIBase getCorrespondingInventoryGUIBase(Inventory inventory) {
+        return instance.openInventoryGUIs.get(inventory);
     }
 
     public static void handleInventoryGUIAction(InventoryGUIAction action) {
-        instance.openInventoryGUIs.stream()
-                .filter(g -> Objects.equals(g, action.getInventoryGUI()))
-                .forEach(g -> g.handleAction(action));
+        instance.openInventoryGUIs.get(action.getInventoryGUI().getInventory()).handleAction(action);
     }
 }
