94d73825a7f96f9bfcd0627328a7c7d332aeba08
Paul Nowitzki <p.nowitzki@tu-bs.de>
2023-01-02 01:32:59 +0100

    completed implementation of InventoryGUIItem

--- src/main/java/net/prismarray/openhivebedwars/gui/InventoryGUIItem.java
+++ src/main/java/net/prismarray/openhivebedwars/gui/InventoryGUIItem.java
@@ -1,37 +1,112 @@
 package net.prismarray.openhivebedwars.gui;
 
-import net.prismarray.openhivebedwars.gui.actions.InventoryGUIClickAction;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
+import java.util.Objects;
 
 public class InventoryGUIItem extends ItemStack {
 
-    private boolean isLocked = true;
-    private final List<InventoryGUIActionListener> actionHandlers = new ArrayList<>();
-
-    public void addClickActionHandler(InventoryGUIActionListener handler) {
-        actionHandlers.add(handler);
+    public InventoryGUIItem() {
+        this(null);
     }
 
-    public void handleClickAction(InventoryGUIClickAction action) {
-        actionHandlers.forEach(
-                l -> InventoryGUIBase.getMatchingHandlers(l, action).forEach(
-                        m -> InventoryGUIBase.invokeMethod(m, l, action)
-                )
+    public InventoryGUIItem(Material material) {
+        this(material, 1);
+    }
+
+    public InventoryGUIItem(Material material, short damage) {
+        this(material, damage, 1);
+    }
+
+    public InventoryGUIItem(Material material, int amount) {
+        this(material, amount, null);
+    }
+
+    public InventoryGUIItem(Material material, short damage, int amount) {
+        this(material, damage, amount, null);
+    }
+
+    public InventoryGUIItem(Material material, int amount, String name) {
+        this(material, amount, name, null);
+    }
+
+    public InventoryGUIItem(Material material, short damage, int amount, String name) {
+        this(material, damage, amount, name, null);
+    }
+
+    public InventoryGUIItem(Material material, int amount, String name, String[] lore) {
+        this(material, amount, name, lore, false);
+    }
+
+    public InventoryGUIItem(Material material, short damage, int amount, String name, String[] lore) {
+        this(material, damage, amount, name, lore, false);
+    }
+
+    public InventoryGUIItem(Material material, int amount, String name, String[] lore, boolean enchanted) {
+        this(material, amount, name, lore, enchanted, null);
+    }
+
+    public InventoryGUIItem(Material material, short damage, int amount, String name, String[] lore, boolean enchanted) {
+        this(material, damage, amount, name, lore, enchanted, null);
+    }
+
+    public InventoryGUIItem(Material material, int amount, String name, String[] lore, boolean enchanted, ItemFlag[] flags) {
+        this(material, (short) 0, amount, name, lore, enchanted, flags);
+    }
+
+    public InventoryGUIItem(
+            Material material,
+            short damage,
+            int amount,
+            String name,
+            String[] lore,
+            boolean enchanted,
+            ItemFlag[] flags
+    ) {
+
+        super(
+                (Objects.isNull(material)) ? Material.AIR : material,
+                amount,
+                damage
         );
-    }
 
-    public void lock() {
-        this.isLocked = true;
-    }
+        if (Objects.equals(getType(), Material.AIR)) {
+            return;
+        }
 
-    public void unlock() {
-        this.isLocked = false;
-    }
+        ItemMeta meta = Bukkit.getItemFactory().getItemMeta(getType());
 
-    public boolean isLocked() {
-        return this.isLocked;
+        if (Objects.nonNull(name)) {
+            meta.setDisplayName(name);
+        }
+
+        if (Objects.nonNull(lore)) {
+            meta.setLore(Arrays.asList(lore));
+        }
+
+        if (enchanted) {
+            meta.addEnchant(Enchantment.getByName("InventoryGUIDummyEnchantment"), 1, true);
+        }
+
+        if (Objects.nonNull(flags)) {
+            meta.addItemFlags(flags);
+
+        } else {
+            meta.addItemFlags(
+                    ItemFlag.HIDE_ENCHANTS,
+                    ItemFlag.HIDE_DESTROYS,
+                    ItemFlag.HIDE_PLACED_ON,
+                    ItemFlag.HIDE_POTION_EFFECTS,
+                    ItemFlag.HIDE_UNBREAKABLE
+            );
+        }
+
+        setItemMeta(meta);
     }
 }
