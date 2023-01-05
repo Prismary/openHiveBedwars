b7a407e95258806fab2d1541f604f62f3686c589
Paul Nowitzki <p.nowitzki@tu-bs.de>
2023-01-05 05:01:45 +0100

    implemented BridgeBuilder, BridgeBuilderItem and BridgeBuilderManager

--- src/main/java/net/prismarray/openhivebedwars/bedwars/bridgebuilder/BridgeBuilder.java
+++ src/main/java/net/prismarray/openhivebedwars/bedwars/bridgebuilder/BridgeBuilder.java
@@ -1,19 +1,193 @@
 package net.prismarray.openhivebedwars.bedwars.bridgebuilder;
 
+import net.prismarray.openhivebedwars.OpenHiveBedwars;
+import net.prismarray.openhivebedwars.util.Broadcast;
+import net.prismarray.openhivebedwars.util.FloatingArmorStand;
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.craftbukkit.v1_8_R3.CraftWorld;
+import org.bukkit.craftbukkit.v1_8_R3.inventory.CraftItemStack;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.util.Vector;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 public class BridgeBuilder {
 
+    /**
+     * Speed of this BridgeBuilder in blocks per second
+     */
+    private static final double SPEED = 5.0; // TODO: move to config variable
+    private static final List<Material> replaceableMaterials = Arrays.asList(
+            Material.DEAD_BUSH,
+            Material.LONG_GRASS
+    ); // TODO: move to config variable or somehow unify with breakable materials?
+
+    private final Material blockType;
+    private final Location spawnLocation;
+    private final Vector direction;
+    private final Vector strictDirection;
+    private final Player owner;
+
     private int remainingBlocks;
-    private Location location;
+    private Location lastPlacedLocation;
+    private FloatingArmorStand entity;
 
 
-    public BridgeBuilder(int remainingBlocks, Location spawnLocation) {
+    public BridgeBuilder(Material blockType, int remainingBlocks, Location spawnLocation, Player owner) {
+
+        this.blockType = blockType;
+        this.spawnLocation = spawnLocation.clone();
+        this.spawnLocation.setPitch(0);
+        this.direction = spawnLocation.getDirection().setY(0.0).normalize();
+        this.strictDirection = calculateStrictDirection(direction);
+        this.owner = owner;
+
         this.remainingBlocks = remainingBlocks;
-        this.location = spawnLocation;
+        this.lastPlacedLocation = null;
+        this.entity = null;
     }
 
-    public void move() {
+    public void spawn() {
 
+        entity = new FloatingArmorStand(spawnLocation.getWorld());
+        lastPlacedLocation = null;
+        entity.setInvisible(true);
+
+        entity.setEquipment(4, CraftItemStack.asNMSCopy(new BridgeBuilderItem(blockType, 0)));
+        entity.setLocation(spawnLocation.getX(), spawnLocation.getY(), spawnLocation.getZ(), spawnLocation.getYaw(), spawnLocation.getPitch());
+
+        ((CraftWorld) spawnLocation.getWorld()).getHandle().addEntity(entity);
+        BridgeBuilderManager.registerBridgeBuilder(this);
+
+        entity.getBukkitEntity().setVelocity(direction.clone().multiply(SPEED / 20));
+    }
+
+
+    public void destroy() {
+        ((CraftWorld) spawnLocation.getWorld()).getHandle().removeEntity(entity);
+        entity = null;
+
+        if (remainingBlocks <= 0) {
+            Broadcast.toPlayer(owner, "I'm done with this shit"); // TODO: display correct message
+            return;
+        }
+
+        BridgeBuilderItem bbItem = new BridgeBuilderItem(blockType, remainingBlocks);
+        Map<Integer, ItemStack> notAdded = owner.getInventory().addItem(bbItem);
+
+        Location location = owner.getLocation();
+        notAdded.values().forEach(i -> location.getWorld().dropItemNaturally(location, i));
+
+        Broadcast.toPlayer(owner, "I'm silly and got stuck. Here's the rest of my blocks."); // TODO: display correct messagea
+    }
+
+
+    public void placeBlock() {
+
+        if (Objects.isNull(entity)) {
+            return;
+        }
+
+        if (remainingBlocks <= 0) {
+            destroy();
+            return;
+        }
+
+        Location location = getLocation().clone();
+        location.setY(location.getY() + 1);
+        location.subtract(direction.clone().multiply(0.5));
+
+        Block block;
+        if (OpenHiveBedwars.getBWConfig().bridgeBuilderUseOptimizedPlacement()) {
+            block = projectToOptimizedPath(location).getBlock();
+        } else {
+            block = location.getBlock();
+        }
+
+        if (!Objects.equals(block.getType(), Material.AIR) && !replaceableMaterials.contains(block.getType())) {
+
+            if (
+                    Objects.nonNull(lastPlacedLocation)
+                    && (
+                        (OpenHiveBedwars.getBWConfig().bridgeBuilderNoCollisionsWithPlayerPlacedBlocks()
+                                && !block.hasMetadata("placedBy"))
+                        || !OpenHiveBedwars.getBWConfig().bridgeBuilderNoCollisionsWithPlayerPlacedBlocks()
+                    )
+            ) {
+                destroy();
+            }
+            return;
+        }
+
+        block.setType(blockType);
+        block.setMetadata("placedBy", new FixedMetadataValue(OpenHiveBedwars.getInstance(), owner.getUniqueId()));
+        remainingBlocks--;
+
+        if (Objects.equals(block.getLocation(), lastPlacedLocation)) {
+            return;
+        }
+        lastPlacedLocation = block.getLocation();
+    }
+
+    public Location getLocation() {
+        return Objects.isNull(entity) ? null : entity.getBukkitEntity().getLocation();
+    }
+
+    public Material getBlockType() {
+        return blockType;
+    }
+
+    public int getRemainingBlocks() {
+        return remainingBlocks;
+    }
+
+    public FloatingArmorStand getEntity() {
+        return entity;
+    }
+
+    private Location projectToOptimizedPath(Location location) {
+
+        if (
+                Objects.nonNull(lastPlacedLocation)
+                && ((strictDirection.getZ() == 0 && location.getBlockX() == lastPlacedLocation.getBlockX())
+                    || (strictDirection.getX() == 0 && location.getBlockZ() == lastPlacedLocation.getBlockZ())
+                    || (
+                            Math.abs(strictDirection.getX()) == Math.abs(strictDirection.getZ())
+                                    && (!Objects.equals(location, lastPlacedLocation.clone().add(strictDirection)))
+                    )
+                )
+        ) {
+            return lastPlacedLocation.clone();
+        }
+
+        return location.clone();
+    }
+
+    private static Vector calculateStrictDirection(Vector direction) {
+
+        Vector temp = direction.clone().normalize();
+
+        if (temp.getX() > Math.abs(direction.getZ())) {
+            return new Vector(1, 0, 0);
+
+        } else if (-temp.getX() > Math.abs(direction.getZ())) {
+            return new Vector(-1, 0, 0);
+
+        } else if (temp.getZ() > Math.abs(direction.getX())) {
+            return new Vector(0, 0, 1);
+
+        } else if (-temp.getZ() > Math.abs(direction.getX())) {
+            return new Vector(0, 0, -1);
+        }
+
+        // return the normalized input vector, if the x and z component are equal in their absolute value
+        return temp;
     }
 }
--- src/main/java/net/prismarray/openhivebedwars/bedwars/bridgebuilder/BridgeBuilderItem.java
+++ src/main/java/net/prismarray/openhivebedwars/bedwars/bridgebuilder/BridgeBuilderItem.java
@@ -1,10 +1,83 @@
 package net.prismarray.openhivebedwars.bedwars.bridgebuilder;
 
-import net.prismarray.openhivebedwars.gui.InventoryGUIPlayerHead;
+import net.prismarray.openhivebedwars.gui.InventoryGUICustomHead;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
 
-public class BridgeBuilderItem extends InventoryGUIPlayerHead {
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-    public BridgeBuilderItem(int stock) {
-        super("steve", 1, String.format("Bridge Builder [%d]", stock));
+public class BridgeBuilderItem extends InventoryGUICustomHead {
+
+    private static final Pattern NAMING_PATTERN = Pattern.compile("(.+) Bridge Builder \\[(\\d+)]");
+
+    private final int remainingBlocks;
+    private final Material blockType;
+
+    public BridgeBuilderItem(Material blockType, int remainingBlocks) {
+        super(getURLForMaterial(blockType), 1, String.format("%s Bridge Builder [%d]", blockType.name(), remainingBlocks));
+        this.remainingBlocks = remainingBlocks;
+        this.blockType = blockType;
+    }
+
+
+    private static String getURLForMaterial(Material material) {
+
+        switch (material) {
+            case WOOL:
+                return "http://textures.minecraft.net/texture/3faf4c29f1e7405f4680c5c2b03ef9384f1aecfe2986ad50138c605fefff2f15";
+            case ENDER_STONE:
+                return "http://textures.minecraft.net/texture/42813cf8c4fd013baf5bf55a8c93121c482da1cf6e1054c180ff3e81727b65dd";
+            case WOOD:
+                return "http://textures.minecraft.net/texture/993993674424e62f0a461c6268f8854541e024c7d3b416a250b2b4d11b50179d";
+            case STONE: // actually andesite, but who's counting...
+                return "http://textures.minecraft.net/texture/adb7bf059a62d27b1e1e2f34394f3f38ed8cda45471f6f4d5b47c3912d181135";
+            case PRISMARINE:
+                return "http://textures.minecraft.net/texture/97e56140686e476aef5520acbabc239535ff97e24b14d87f4982f13675c";
+            case OBSIDIAN:
+                return "http://textures.minecraft.net/texture/4de719b72909efa097815a63380f4456af9e4afebdd894e5b58b7c9e05675577";
+            case BEDROCK:
+                return "http://textures.minecraft.net/texture/bf7a422db35d28cfb67e6c1615cdac4d73007247187740ba8653899a44b7b520";
+            default: // also Material.COMMAND
+                return "http://textures.minecraft.net/texture/5f4c21d17ad636387ea3c736bff6ade897317e1374cd5d9b1c15e6e8953432";
+        }
+    }
+
+    public static BridgeBuilderItem fromItemStack(ItemStack item) {
+
+        if (!Objects.equals(item.getType(), Material.SKULL_ITEM)) {
+            return null;
+        }
+
+        if (!item.hasItemMeta() || Objects.isNull(item.getItemMeta().getDisplayName())) {
+            return null;
+        }
+
+        Matcher matcher = NAMING_PATTERN.matcher(item.getItemMeta().getDisplayName());
+
+        if (!matcher.matches()) {
+            return null;
+        }
+
+        Material material;
+        int remainingBlocks;
+        try {
+            material = Material.valueOf(matcher.group(1));
+            remainingBlocks = Integer.parseInt(matcher.group(2));
+
+        } catch(IllegalStateException | IndexOutOfBoundsException | IllegalArgumentException e) {
+            return null;
+        }
+
+        return new BridgeBuilderItem(material, remainingBlocks);
+    }
+
+    public int getRemainingBlocks() {
+        return remainingBlocks;
+    }
+
+    public Material getBlockType() {
+        return blockType;
     }
 }
